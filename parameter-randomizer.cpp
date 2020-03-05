0ab4e49ee8a12ec754d608b4162647b0a18f37e1
TnAgMutNjaTurtl <joseph.d.pope@gmail.com>
2020-03-04 20:44:18 -0600

    para ran with correct returns

--- parameter-randomizer.cpp
+++ parameter-randomizer.cpp
@@ -9,10 +9,11 @@ int main(int argc, char **argv)
     if (argc == number)
     {
         cout << "That's correct! I take " << number << " parameters!" << endl;
+        return 0;
     }
     if (argc != number)
     {
         cout << "You have guessed incorrectly. I take " << number << " parameters. Do not pass go and do not collect $200." << endl;
+        return 1;
     }
-    return 0;
 }
