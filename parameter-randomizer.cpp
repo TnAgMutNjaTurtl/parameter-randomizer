df9b3586212ce9c9cacb72c6afab83a065c77e3b
TnAgMutNjaTurtl <joseph.d.pope@gmail.com>
2020-03-04 20:41:30 -0600

    the param ran

---
+++ parameter-randomizer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL));
+    int number = rand() % 4 + 1;
+    if (argc == number)
+    {
+        cout << "That's correct! I take " << number << " parameters!" << endl;
+    }
+    if (argc != number)
+    {
+        cout << "You have guessed incorrectly. I take " << number << " parameters. Do not pass go and do not collect $200." << endl;
+    }
+    return 0;
+}
