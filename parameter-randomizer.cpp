741728453bcd94914d16d4587127fd4c738efef7
TnAgMutNjaTurtl <joseph.d.pope@gmail.com>
2020-03-04 20:49:10 -0600

    the best if layout

--- parameter-randomizer.cpp
+++ parameter-randomizer.cpp
@@ -6,14 +6,13 @@ int main(int argc, char **argv)
 {
     srand(time(NULL));
     int number = rand() % 4 + 1;
-    if (argc == number)
-    {
-        cout << "That's correct! I take " << number << " parameters!" << endl;
-        return 0;
-    }
+    
     if (argc != number)
     {
         cout << "You have guessed incorrectly. I take " << number << " parameters. Do not pass go and do not collect $200." << endl;
         return 1;
     }
+
+    cout << "That's correct! I take " << number << " parameters!" << endl;
+    return 0;
 }
